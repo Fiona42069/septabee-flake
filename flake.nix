97f83ce0bd5ded18ea3f42746f148a7d6f6f22d2
APnda <apnda6661@proton.me>
2026-09-02 23:05:50 -0500

    update to yeet-44
    remove hacky jit runtime install

--- flake.nix
+++ flake.nix
@@ -24,20 +24,14 @@
       zstd
       ncurses
     ];
-    version = "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1-alpha-beta-gamma-delta";
-
-    llvm-and-stuff = pkgs.fetchurl { 
-      # Unofficial link. the actual library is unobtainable (custom format archive)
-      url = "https://ubq323.website/files/libseptabee-jit-runtime.so";
-      sha256 = "sha256-utsRjtNDr0CiVDeS7974jjMXPz/GmYC6GIVrI80y0hM=";
-    };
+    version = "yeet-44";
 
     septabee-pkg = pkgs.stdenv.mkDerivation {
         name = "septabee-${version}";
         version = version;
         src = pkgs.fetchurl {
-          url = "https://septabee.nekoweb.org/important_stuff/SEPTABEE_DOWNLOADS/version_B/septabee_linux_B_T1.7z";
-          sha256 = "sha256-JlWmeDnMTjBNwLTADvSswbtfhJK6t1bu0xHkmBgLtvA=";
+          url = "https://septabee.nekoweb.org/important_stuff/SEPTABEE_DOWNLOADS/version_B/septabee_linux_B_T2.7z";
+          sha256 = "sha256-OMnbRBTku8yi4b3Ay7d70EbB/e2Qh+PfzK2O8qRFoaA=";
         };
 
         nativeBuildInputs = with pkgs; [
@@ -56,10 +50,8 @@
 
         installPhase = ''
           runHook preInstall
-          mkdir -p "$out/bin" "$out/lib" # "exec" "$out/lib/llvm-stuffs/abi-8"
+          mkdir -p "$out/bin" 
           cp -r ./linux/* "$out/bin/"
-          install -Dm755 "${llvm-and-stuff}" \
-            "$out/lib/llvm-stuffs/abi-8/libseptabee-jit-runtime.so"
           runHook postInstall
         '';
 
@@ -71,10 +63,6 @@
               abi_dir=\"\$data_home/Septabee/llvm-stuffs/abi-8\"
 
               mkdir -p \"\$abi_dir\"
-
-              ln -sfn \
-                \"$out/lib/llvm-stuffs/abi-8/libseptabee-jit-runtime.so\" \
-                \"\$abi_dir/libseptabee-jit-runtime.so\"
             "
         '';
     };
